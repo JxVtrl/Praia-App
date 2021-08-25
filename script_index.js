3c90840f0be3a0b522766bc01d1736842cc0fe3b
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-08-25 17:16:49 -0300

    :zap: new assets

--- script_index.js
+++ script_index.js
@@ -51,7 +51,7 @@ function findLocation(position){
 
 // Achar cidade API Google 
 async function getCity(latitude, longitude){
-    const cidade_URL = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyCosb0MEIu5QMg8UhvUt0atgr5P90PSae8`
+    const cidade_URL = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&ey=AIzaSyCosb0MEIu5QMg8UhvUt0atgr5P90PSae8`
     let response = await fetch(cidade_URL)
     let json = await response.json()
     console.log(json)
