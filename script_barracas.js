b5c1d87ffb513a841b1e2e555b423335ca2f9ab1
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-08-27 07:08:26 -0300

    :zap: map On Demand

--- script_barracas.js
+++ script_barracas.js
@@ -28,6 +28,24 @@ function formatHora(hora){
 /////////////////////////////////////////////////////////////////////////////////////////
 //                                          MAPS
 
+// Achar localizacao
+getLocation()
+function getLocation(){
+    navigator.geolocation.getCurrentPosition(findLocation, showError, {enableHighAccuracy:true,maximumAge:600000})
+}
+
+
+function showError(error) {
+    alert(error.code + ' ' + error.message);
+}
+
+function findLocation(position){
+    const latitude  = position.coords.latitude
+    const longitude = position.coords.longitude
+    initMap(latitude, longitude)
+}
+
+
 // Clique do Botão
 document.getElementById('abrir-mapa').addEventListener('click', function(){
     if(document.getElementById('map-container').classList.contains('hide')){
@@ -44,37 +62,147 @@ document.getElementById('abrir-mapa').addEventListener('click', function(){
 
 // Iniciando o Maps
 function initMap(latitude, longitude) {
-    var localizacao = {lat:`${latitude}`, lng:`${longitude}`}
-
-    var options = {
-        zoom:16,
-        center: {lat:-22.9769216, lng:-43.2242688},
+    const localizacao = {lat:latitude, lng:longitude}
+    
+    
+    let options = {
+        zoom:13,
+        center: {lat:-22.983105686081743, lng:-43.211015324246624},
+        mapTypeControl: false,
+        disableDefaultUI: true,
+        fullscreenControl: true,
+        zoomControl: true,
+        zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.SMALL,
+            position: google.maps.ControlPosition.RIGHT_BOTTOM
+        },
+        fullscreenControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_TOP
+        },
+        styles: [
+            {
+                "featureType": "all",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "saturation": "32"
+                    },
+                    {
+                        "lightness": "-3"
+                    },
+                    {
+                        "visibility": "on"
+                    },
+                    {
+                        "weight": "1.18"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.man_made",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "saturation": "-70"
+                    },
+                    {
+                        "lightness": "14"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "saturation": "100"
+                    },
+                    {
+                        "lightness": "-14"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    },
+                    {
+                        "lightness": "12"
+                    }
+                ]
+            }
+        ]
     }
 
-    var map = new google.maps.Map(document.getElementById('map'), options);
+    // Criando o mapa
+    const map = new google.maps.Map(document.getElementById('map'), options);
 
-    /*
-    var marker = new google.maps.Marker({
-        position: localizacao,
-        map: map,
-        title: 'Barracas'
-    });
-    */
+    // Lista de Marcadores
+    const marcadores = [
+        {
+            content: '<h2>Posto 12</h2>',
+            coords:{lat:-22.98783021016635, lng:-43.225977817476455},
+            icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+        },
+        {
+            content: '<h2>Casa</h2>',
+            coords:{lat:-22.97416497753591, lng:-43.22750406210507},
+            icon: 'assets/icon/location.png' // ICONE DE 32PX
+        }
+
+    ]
+
+    // Adicionar todos os marcadores ao mapa
+    for(let i = 0; i < marcadores.length; i++){
+        addMarker(marcadores[i])
+    }
+
+    function addMarker(props, title){
+        let marker = new google.maps.Marker({
+            position: props.coords,
+            map: map,
+            content: `<h2>${title}</h2>`
+        })
+
+        // Verifica se tiver icone diferente
+        if(props.icon){
+            marker.setIcon(props.icon)
+        }
+
+        // Verifica se tiver conteúdo do marcador
+        if(props.content){
+            let infowindow = new google.maps.InfoWindow({
+                content: props.content   
+            });
+            
+            marker.addListener('click', function() {
+                infowindow.open(map, marker);
+            })
+        }
+
+    }
+
+
 }
 
-getLocation()
-// Achar localizacao
-function getLocation(){
-    navigator.geolocation.getCurrentPosition(findLocation, showError, {enableHighAccuracy:true,maximumAge:600000})
-}
-
-
-function showError(error) {
-    alert(error.code + ' ' + error.message);
-}
-
-function findLocation(position){
-    const latitude  = position.coords.latitude
-    const longitude = position.coords.longitude
-    initMap(latitude, longitude)
-}
